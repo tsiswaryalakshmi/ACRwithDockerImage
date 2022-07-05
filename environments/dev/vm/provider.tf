dcc13f805d72051dfe88dac1ce42b2b166a21ce6
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-07-05 12:00:20 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -1,20 +1,19 @@
 terraform {
   required_providers {
     azurerm = ">=2.52.0"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
   }
+
   backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
 }
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
+
 }
 
 provider "docker" {}
